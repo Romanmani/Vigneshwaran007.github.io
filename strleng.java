773873ce0738f5e67600c1300dfe47fa22db9a63
Vigneshwaran007 <vigneshwaranown@gmail.com>
2016-07-28 18:02:06 +0530

    Add files via upload

---
+++ strleng.java
@@ -0,0 +1,16 @@
+package String;
+
+public class strleng {
+public static void main(String[] args) {
+String s="praveen";
+int len1=5;
+if(s.length()>len1)
+{
+	System.out.println("length is above 5");
+}
+else
+{
+	System.out.println("length is below 5");
+}
+}
+}
