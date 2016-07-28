7f5e38df031745ec2b11cbe1456b25fc719e45b8
Vigneshwaran007 <vigneshwaranown@gmail.com>
2016-07-28 18:06:57 +0530

    Add files via upload

---
+++ String_To_Integer.java
@@ -0,0 +1,12 @@
+package String;
+
+public class String_To_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+       Integer n1=Integer.valueOf("12345");
+       System.out.println(n1);
+	}
+
+}
